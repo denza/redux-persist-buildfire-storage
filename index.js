4325e84fb6d76cfe7b9b572cd109d51e5268fd6f
Milos Kapovic <miloskapovic@gmail.com>
2019-08-08 19:03:14 +0200

    changes

--- index.js
+++ index.js
@@ -1,67 +1,79 @@
+
 export default class BuildFireStorage {
     constructor(storageDirectory, actionTypes) {
-        this.storageDirectory = storageDirectory;
-        this.actionTypes = actionTypes;
+      this.storageDirectory = storageDirectory;
+      this.actionTypes = actionTypes;
     }
-
+  
     getItem(key) {
-        return new Promise((resolve, reject) => {
-            try {
-                var options = {
-                    path: this.storageDirectory
-                    , fileName: key
-                }
-
-                function gotContent(error, fileContent) {
-                    if(error)reject(error);
-                    resolve(JSON.parse(fileContent));
-                }
-
-                buildfire.services.fileManager.readFileAsText(options, gotContent);
-            } catch (e) {
-                reject(e)
-            }
-        })
+      return new Promise((resolve, reject) => {
+        try {
+          let options = {
+            path: this.storageDirectory,
+            fileName: key
+          };
+  
+          function gotContent(error, fileContent) {
+            if (error)reject(error);
+            resolve(JSON.parse(fileContent));
+          }
+  
+          window.buildfire.services.fileManager.readFileAsText(options, gotContent);
+        } catch (e) {
+          reject(e);
+        }
+      });
     }
+  
     setItem(key, value) {
-        return new Promise((resolve, reject) => {
-            try {
-                var options = {
-                    path: this.storageDirectory
-                    , fileName: key
-                    , content: value
-                }
-
-                function fileWritten(error) {
-                    if (error)
-                        console.error("failed to write file due to ", error);
-                    else
-                        console.log("File Written");
-                }
-
-                buildfire.services.fileManager.writeFileAsText(options, fileWritten);
-            } catch (e) {
-                reject(e)
-            }
-        })
+      return new Promise((resolve, reject) => {
+        try {
+          let options = {
+            path: this.storageDirectory,
+            fileName: key,
+            content: value
+          };
+  
+          function fileWritten(error, flag) {
+            if (error)reject(error);
+            resolve(JSON.parse(flag));
+          }
+  
+          window.buildfire.services.fileManager.writeFileAsText(options, fileWritten);
+        } catch (e) {
+          reject(e);
+        }
+      });
     }
+  
     removeItem(key) {
-        return new Promise((resolve, reject) => {
-            try {
-                //   this.localStorage.setItem(key, value)
-                //   process.nextTick(() => resolve())
-            } catch (e) {
-                reject(e)
-            }
-        })
+      return new Promise((resolve, reject) => {
+        try {
+          let options = {
+            path: this.storageDirectory,
+            fileName: key
+          };
+  
+          function fileDeleted(error, successFlag) {
+            if (error)reject(error);
+            resolve(JSON.parse(successFlag));
+          }
+  
+          buildfire.services.fileManager.deleteFile(options, fileDeleted);
+        } catch (e) {
+          reject(e);
+        }
+      });
     }
+  
     getAllKeys() {
-        return new Promise((resolve, reject) => {
-            try {
-                return this.actionTypes;
-            } catch (e) {
-                reject(e)
-            }
-        })
+      return new Promise((resolve, reject) => {
+        try {
+          return this.actionTypes;
+        } catch (e) {
+          reject(e);
+        }
+      });
     }
-}
+  }
+  
